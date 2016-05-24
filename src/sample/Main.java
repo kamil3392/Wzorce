4181d268a3d58e76b4d6cc95a3ea6169533326bd
Dixeer <damian.zmuda22@gmail.com>
2016-05-24 18:33:51 +0200

    wersja 4

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("wersja 3");
+        primaryStage.setTitle("wersja 4");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
