d6e1f662f231a1ce928012aedbca2db8a930a730
Dixeer <damian.zmuda22@gmail.com>
2016-05-28 16:31:31 +0200

    wersja 4

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,18 +2,41 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Box;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("wersja 6");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Drawing Text");
+        Group root = new Group();
+        Scene scene = new Scene(root, 500, 300 , Color.WHITE);
+        int red = 30;
+        int green = 40;
+        int blue = 50;
+
+        Text text = new Text(250, 20, "Menu");
+        text.setWrappingWidth(200);
+        text.setTextAlignment(TextAlignment.CENTER);
+        Box box = new Box();
+
+
+
+        text.setFill(Color.rgb(red, green, blue, .999));
+        root.getChildren().add(text);
+
+
+        primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 
 
